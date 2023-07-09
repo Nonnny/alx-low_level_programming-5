7f1d9414e22651fc0277e790d2bd1402affad7ee
Nonnny <elsienonny@gmail.com>
2023-07-09 04:29:50 -0700

    correction task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - value of a bit at an index in a decimal no
  * @n: no to search
  * @index: index of the bit
- * Return_ value of the bit index else -1
+ * Return: value of the bit index else -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
