ddd51d134bfcad563fe834adcefd25c344513c4e
Nonnny <elsienonny@gmail.com>
2023-07-11 08:25:09 -0700

    task 1-2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file- text to append at the end of the file
+ * @filename: name of the file
+ * @text_content: string terminated at end of file
+ * Return: if successful 1 else -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
