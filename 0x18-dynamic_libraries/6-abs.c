3bdaca2dd5939375df3c1b150c800f6bd473a5d1
Nonnny <elsienonny@gmail.com>
2023-08-27 10:22:48 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - contains the value of an integer
+ * @x: Computes the integer
+ * Return: The exact value
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}
